fa6817d7d5d33324d0b3c31dc165add8b027e6fd
rezainiet <masudrezaog@gmail.com>
2022-04-18 22:47:38 +0600

    about me page completed

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import './About.css'
 
 const About = () => {
     return (
-        <div>
-            <h2>This is about</h2>
+        <div className='bg-dark'>
+            <div id="cards_landscape_wrap-2">
+                <div class="container">
+                    <div class="row d-flex align-items-center justify-content-center">
+                        <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                            <div class="card-flyer">
+                                <div class="text-box">
+                                    <div class="image-box">
+                                        <img src="https://i.ibb.co/qRJ0q84/img2.jpg" alt="" />
+                                    </div>
+                                    <div class="text-container">
+                                        <h6>Masud Reza</h6>
+                                        <p>অনেক ছোট বেলা থেকেই স্বপ্ন ছিল আইটি রিলেটেড কোন কিছু করব। এখন সেটা বাস্তবায়ন করার যথেষ্ট চেষ্টা করছি। আর সারাজীবন করতে থাকব। </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <p className='text-white mt-5'>আমি নিজেই বুঝতে পারি আমি একজন ফাঁকিবাজ ছেলে। কোন কাজ-ই ঠিক মত সময় দিয়ে করতে পারি না। কিন্তু কোডিং করতে বসলে মনে হয় না যে কষ্ট হচ্ছে। যদিও আমি প্রব্লেম সল্ভিং এ খুব একটা পারদর্শী না কিন্তু তবুও অনেক ইচ্ছা আছে ভবিষ্যতে ভালো মানের সফটওয়্যার ইঞ্জিনিয়ার হব। <br />
+                        লক্ষ্য হচ্ছে গুগল এর মত বড় টেক জায়েন্ট কোম্পানিতে জব করার সুযোগ না পেলেও এরকম ভালো কোন কোম্পানিতে জব করার। <br />আমি ডিপ্লোমা-ইঞ্জিনিয়ারিং পড়তেছি। এই জন্য পড়াশুনার খুব চাপ থাকলেও প্রোগ্রামিং এ কম্প্রোমাইজ করব না ভেবে নিয়েছি। পরবর্তী ৬ মাস এর মধ্যেই কোন না কোন কোম্পানির সাথে কাজ করার ইচ্ছা অনেক প্রবল ।</p>
+                </div>
+            </div>
+
         </div>
     );
 };
